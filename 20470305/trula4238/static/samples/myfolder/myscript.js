a01360cad16a0b5cab0abd862ca5f59b42727099
davidharman <davidharman@uk.ibm.com>
2017-04-27 17:03:26 +0100

    Update myscript.js

--- 20470305/trula4238/static/samples/myfolder/myscript.js
+++ 20470305/trula4238/static/samples/myfolder/myscript.js
@@ -1,6 +1,6 @@
 if(typeof(dojo) != "undefined") {
     dojo.place(
-        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/app/file/a9f76908-4751-4c09-9c45-2bca4487ceba\"></link>",
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"//files/muse-static/samples/myfolder/mine.css\"></link>",
         dojo.doc.head,
         "last"
     );
