3f9d612260dabfd70008effe3d9ab3356ec0dd5b
davidharman <davidharman@uk.ibm.com>
2017-04-27 17:16:23 +0100

    Update myscript.js

--- 20470305/trula4238/static/samples/myfolder/myscript.js
+++ 20470305/trula4238/static/samples/myfolder/myscript.js
@@ -1,3 +1,9 @@
+////
+// @author David Harman, IBM
+// @name Homepage Customization
+// @version 0.1
+// @date April, 2017
+//
 if(typeof(dojo) != "undefined") {
     dojo.place(
         "<link rel=\"stylesheet\" type=\"text/css\" href=\"//files/muse-static/samples/myfolder/mine.css\"></link>",
